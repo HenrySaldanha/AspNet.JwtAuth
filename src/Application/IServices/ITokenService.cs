d2a2151e178ccddff43bb268e11821cbec2ec62c
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-07-03 17:30:50 -0300

    Add service to generate token

---
+++ src/Application/IServices/ITokenService.cs
@@ -0,0 +1,7 @@
+﻿using Domain.Entities;
+
+namespace Application.IServices;
+public interface ITokenService
+{
+    public string GenerateToken(User user);
+}
---
+++ src/Application/Services/TokenService.cs
@@ -0,0 +1,41 @@
+﻿using Application.IServices;
+using Application.Options;
+using Domain.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Application.Services;
+public class TokenService : ITokenService
+{
+    private readonly TokenSecretOptions _options;
+
+    public TokenService(IOptions<TokenSecretOptions> options)
+    {
+        _options = options.Value;
+    }
+
+    public string GenerateToken(User user)
+    {
+        Log.Information($"Generating Token for user {user.Name}");
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_options.Secret);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddMinutes(1),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            Subject = new ClaimsIdentity(new[]
+            {
+                    new Claim(ClaimTypes.Name, user.Name),
+                    new Claim(ClaimTypes.Role, user.Role.ToString())
+            })
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
